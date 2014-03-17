38c0bddde490022eedf7e68d4e0a953f71f5f03e
Ivan Kerin <ikerin@gmail.com>
2014-03-17 15:31:50 +0200

    Add README

---
+++ README.md
@@ -0,0 +1,11 @@
+Package Template
+================
+
+Use this to create a new clippings package
+
+First create a new github repository
+
+Then:
+
+	composer create-project clippings/package-template foobar
+
--- install.php
+++ install.php
@@ -27,6 +27,7 @@ $placeholders = array(
 $allItems = new RecursiveIteratorIterator(new RecursiveDirectoryIterator('dist'));
 
 unlink('composer.json');
+unlink('README.md');
 
 foreach ($allItems as $file) {
     file_put_contents($file->getPathname(), strtr(file_get_contents($file->getPathname()), $placeholders));
