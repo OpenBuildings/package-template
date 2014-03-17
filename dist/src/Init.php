1dbc9e66eb28b7434e87f73b047fcd3453befa8f
Ivan Kerin <ikerin@gmail.com>
2014-03-17 16:31:14 +0200

    Add initial class

---
+++ dist/src/Init.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CL\%%REPO_NAMESPACE%%;
+
+/**
+ * @author    %%AUTHOR_NAME%% <%%AUTHOR_EMAIL%%>
+ * @copyright (c) %%YEAR%% Clippings Ltd.
+ * @license   http://spdx.org/licenses/BSD-3-Clause
+ */
+class Init
+{
+
+}
